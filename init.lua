7847e5027f9486fee9279ca8da2ea9d080fcef24
Smartkin <vladislav5019@gmail.com>
2023-05-09 22:31:51 +0300

    Add most of the layout images. Start building 1p 4x3 no cam layout

--- init.lua
+++ init.lua
@@ -3,9 +3,7 @@
 -- To output to OBS script log window use Lua's `print` function
 require("layout_source")
 require("util")
-obs = obslua
-
-
+local obs = obslua
 
 local description = [[
     <center><h2>Fangame Marathon 2023 Layout Program v1</h2></center>
@@ -14,10 +12,13 @@ local description = [[
     <p>
     After creating the layouts DO NOT change the name of any elements in the scenes!
     This WILL break the program!
+    <p>
+    You can, after creating the layouts, add new elements if you need to but they won't be automated by the Dashboard provided for each scene
 ]]
 
 function create_layouts()
     obs.script_log(obs.LOG_INFO, "Creating layouts...")
+    create_1p_no_cam_4x3_layout()
     create_3p_4x3_layout()
 end
 
@@ -31,15 +32,34 @@ function script_description()
     return description
 end
 
+function create_1p_no_cam_4x3_layout()
+    local new_scene = util.create_scene("FM 1 person no cam 4x3 layout")
+    local runner_1_text = util.create_text_eaves(new_scene, "Cosmoing", 32, "center", 0xFF5500,
+        util.source_names.runner_1, 222, 494)
+    local runner_1_pronouns = util.create_text_eaves(new_scene, "He/Him", 18, "left", 0xFFFFFF,
+        util.source_names.runner_1_pronouns, 422, 502)
+    local layout_data = obs.obs_data_create()
+    obs.obs_data_set_string(layout_data, util.setting_names.r1_source, runner_1_text)
+    obs.obs_data_set_string(layout_data, util.setting_names.r1_pr_source, runner_1_pronouns)
+    local layout_source = obs.obs_source_create(layout_1p_no_cam_4x3_source_def.id, "Dashboard", layout_data, nil)
+    obs.obs_scene_add(new_scene, layout_source)
+    local layout_item = obs.obs_scene_sceneitem_from_source(new_scene, layout_source)
+    obs.obs_sceneitem_set_order(layout_item, obs.OBS_ORDER_MOVE_BOTTOM)
+    obs.obs_sceneitem_release(layout_item)
+    obs.obs_data_release(layout_data)
+    obs.obs_source_release(layout_source)
+    obs.obs_scene_release(new_scene)
+end
+
 function create_3p_4x3_layout()
     local new_scene = util.create_scene("FM 3 person 4x3 layout")
-    local runner_1_text = util.create_text_eaves(new_scene, "Cosmoing", 32, "left", 0xFF5500, util.source_names.runner_1,
-        705, 20)
+    local runner_1_text = util.create_text_eaves(new_scene, "Cosmoing", 32, "left", 0xFF5500,
+        util.source_names.runner_1, 705, 20)
     local runner_1_pronouns = util.create_text_eaves(new_scene, "He/Him", 18, "left", 0xFFFFFF,
         util.source_names.runner_1_pronouns, 720, 53)
     local layout_data = obs.obs_data_create()
-    obs.obs_data_set_string(layout_data, "runner_1_text_source", runner_1_text)
-    obs.obs_data_set_string(layout_data, "runner_1_pronouns_source", runner_1_pronouns)
+    obs.obs_data_set_string(layout_data, util.setting_names.r1_source, runner_1_text)
+    obs.obs_data_set_string(layout_data, util.setting_names.r1_pr_source, runner_1_pronouns)
     local layout_source = obs.obs_source_create(layout_3p_4x3_source_def.id, "Dashboard", layout_data, nil)
     obs.obs_scene_add(new_scene, layout_source)
     local layout_item = obs.obs_scene_sceneitem_from_source(new_scene, layout_source)
--- util.lua
+++ util.lua
@@ -1,4 +1,6 @@
-obs = obslua
+local table_to_string = require("table_to_string")
+local obs = obslua
+
 util = {}
 
 util.image_source_load = function(image, file)
@@ -17,19 +19,53 @@ util.image_source_load = function(image, file)
     end
 end
 
+-- This function returns an OPEN handler for the scene so you have to manually free it
+-- using `obs.obs_scene_release`
 util.create_scene = function(scene_name)
     local new_scene = obs.obs_scene_create(scene_name)
 
     return new_scene
 end
 
+util.layout_ctx = {}
+
 util.source_names = {
     runner_1 = "Runner 1",
-    runner_1_pronouns = "Runner 1 Pronouns"
+    runner_1_pronouns = "Runner 1 Pronouns",
+    runner_2 = "Runner 2",
+    runner_2_pronouns = "Runner 2 Pronouns",
+    runner_3 = "Runner 3",
+    runner_3_pronouns = "Runner 3 Pronouns"
+}
+
+util.dashboard_names = {
+    r1_name = "Runner 1 name",
+    r1_pr = "Runner 1 pronouns",
+    r2_name = "Runner 2 name",
+    r2_pr = "Runner 2 pronouns",
+    r3_name = "Runner 3 name",
+    r3_pr = "Runner 3 pronouns",
+}
+
+util.setting_names = {
+    r1_source = "runner_1_text_source",
+    r1_pr_source = "runner_1_pronouns_source",
+    r1_name = "runner_1_name",
+    r1_pr = "runner_1_pronouns",
+    r2_source = "runner_2_text_source",
+    r2_pr_source = "runner_2_pronouns_source",
+    r2_name = "runner_2_name",
+    r2_pr = "runner_2_pronouns",
+    r3_source = "runner_3_text_source",
+    r3_pr_source = "runner_3_pronouns_source",
+    r3_name = "runner_3_name",
+    r3_pr = "runner_3_pronouns"
 }
 
 -- For color OBS uses BGR so for blue use 0xFF0000 instead of 0x0000FF
 util.create_text = function(face, size, style, text, align, color, name, scene, x, y)
+    local uuid = nil
+
     local pos = obs.vec2()
 
     local text_settings = obs.obs_data_create()
@@ -53,23 +89,38 @@ util.create_text = function(face, size, style, text, align, color, name, scene, 
         obs.obs_sceneitem_set_pos(text_sceneitem, text_location)
     end
 
+    uuid = obs.obs_source_get_uuid(text_source)
+
     obs.obs_source_update(text_source, text_settings)
     obs.obs_data_release(text_settings)
     obs.obs_data_release(text_font_object)
     obs.obs_source_release(text_source)
 
-    return name
+    return uuid
 end
 
 util.create_text_eaves = function(scene, text, size, align, color, name, x, y)
     return util.create_text("MrEavesXLModOT-Reg", size, "Regular", text, align, color, name, scene, x, y)
 end
 
-util.set_obs_text_source_text = function(name, text)
-    local source = obs.obs_get_source_by_name(name)
+util.set_obs_text_source_text = function(uuid, text)
+    local source = obs.obs_get_source_by_uuid(uuid)
     local settings = obs.obs_data_create()
     obs.obs_data_set_string(settings, "text", text)
     obs.obs_source_update(source, settings)
     obs.obs_data_release(settings)
     obs.obs_source_release(source)
+end
+
+util.create_layout_ctx = function(layout_id)
+    util.layout_ctx[layout_id] = {
+        props_def = nil,
+        props_settings = nil,
+        source_uuids = {}
+    }
+    obs.script_log(obs.LOG_INFO, "Created layout context " .. table_to_string.convert(util.layout_ctx[layout_id]))
+end
+
+util.get_layout_ctx = function(layout_id)
+    return util.layout_ctx[layout_id]
 end
